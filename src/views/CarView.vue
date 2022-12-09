9fd280a18b7ee1d22a2753ee24fa74b7c49b8b0b
LucasPerezDev <LucasPerezDev@gmail.com>
2022-12-09 04:59:14 -0500

    Made a car viewing app to learn routing on vue. Implemented vue useRoute, RouterView and RouterLink for its various usecases

---
+++ src/views/CarView.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import {useRoute, RouterView} from "vue-router";
+  import cars from "../data/cars.json";
+
+  const route = useRoute()
+  const car = cars.find(car => car.id === parseInt(route.params.id))
+  
+</script>
+
+<template>
+  <div>
+    <h1>{{car.name}}</h1>
+    <p>Year: {{car.year}}</p>
+    <p>Price: ${{car.price}}</p>
+    <RouterView  />
+  </div>
+</template>
