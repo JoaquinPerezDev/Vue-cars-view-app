a83583c9f6d610f9c0e8f5ff1d1c97242a429a3b
LucasPerezDev <LucasPerezDev@gmail.com>
2022-12-13 16:23:26 -0500

    Added nested routing for the car owner's contact information, adding the information in each of the car objects in the cars.json, added useRouter to the carviews view and learned about all the different information you can extract with useRoute and all the different methods you can use with useRouter

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,9 +1,11 @@
 <script setup>
-  import {useRoute, RouterView} from "vue-router";
-  import cars from "../data/cars.json";
+  import {useRoute, useRouter, RouterView} from "vue-router"
+  import cars from "../data/cars.json"
 
   const route = useRoute()
-  const car = cars.find(car => car.id === parseInt(route.params.id))
+  const router = useRouter()
+  const carId = parseInt(route.params.id)
+  const car = cars.find(car => car.id === carId)
   
 </script>
 
@@ -12,6 +14,7 @@
     <h1>{{car.name}}</h1>
     <p>Year: {{car.year}}</p>
     <p>Price: ${{car.price}}</p>
+    <button @click="router.push(`/cars/${carId}/contact`)">Click for contact</button>
     <RouterView  />
   </div>
 </template>
